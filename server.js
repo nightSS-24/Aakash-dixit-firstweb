7acf06a9cc8b64207f7f236fcd531e0137eee171
nightSS-24 <meenakshi9958813860@gmail.com>
2026-05-01 21:41:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname)));
 
 // MongoDB Connection
